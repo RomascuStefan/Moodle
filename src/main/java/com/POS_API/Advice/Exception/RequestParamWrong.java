6fb8836c986f1fa90051a6aa0b7e134ebbd4c9e3
Stefan <stefanromascu030405@gmail.com>
2024-11-12 18:58:40 +0200

    Custom Exception for wrong RequestParams

---
+++ src/main/java/com/POS_API/Advice/Exception/RequestParamWrong.java
@@ -0,0 +1,19 @@
+package com.POS_API.Advice.Exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class RequestParamWrong extends RuntimeException {
+
+    private final String parameter;
+    private final String value;
+
+    @Override
+    public String getMessage() {
+        return String.format("Invalid value '%s' for parameter '%s'", value, parameter);
+    }
+}
+
+
