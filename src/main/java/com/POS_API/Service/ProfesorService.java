8cd712ce5d5de39e40a28bf7d04af3b5960a8b81
Stefan <stefanromascu030405@gmail.com>
2024-11-18 23:04:48 +0200

    Added function that filters Profesor by an studii

--- src/main/java/com/POS_API/Service/ProfesorService.java
+++ src/main/java/com/POS_API/Service/ProfesorService.java
@@ -1,12 +1,11 @@
 package com.POS_API.Service;
 
+import com.POS_API.Advice.Exception.RequestParamWrong;
 import com.POS_API.DTO.ProfesorDTO;
-import com.POS_API.Advice.Exception.EnumException;
 import com.POS_API.Advice.Exception.ResourceNotFoundException;
 import com.POS_API.Advice.Exception.UniqueKeyException;
 import com.POS_API.Mapper.ProfesorMapper;
 import com.POS_API.Model.Enums.GradDidactic;
-import com.POS_API.Model.Enums.TipAsociere;
 import com.POS_API.Model.Profesor;
 import com.POS_API.Repository.ProfesorDAO;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,59 +25,75 @@ public class ProfesorService {
         this.profesorRepo = profesorRepo;
     }
 
-    public List<Profesor> findAllProfesori() {
+    public List<ProfesorDTO> findAllProfesori() {
         List<Profesor> profesori = profesorRepo.findAll();
-        if (profesori.isEmpty()) {
-            throw new ResourceNotFoundException("Profesor", "all", "no records found");
-        }
-        return profesori;
+
+        return ProfesorMapper.listEntityToDTO(profesori);
     }
 
-    public Profesor findProfesorById(int id) {
+    public ProfesorDTO findProfesorById(int id) {
         Optional<Profesor> profesor = profesorRepo.findById(id);
+        
         if (profesor.isEmpty()) {
             throw new ResourceNotFoundException("Profesor", "id", id);
         }
-        return profesor.get();
+        return ProfesorMapper.toDTO(profesor.get());
     }
 
-    public List<Profesor> filterProfesoriByRank(List<Profesor> profesori, String acad_rank) {
+    public List<ProfesorDTO> filterProfesoriByRank(List<ProfesorDTO> profesoriDTO, String acad_rank) {
         try {
             GradDidactic gradDidactic = GradDidactic.valueOf(acad_rank);
+
+            List<Profesor> profesori = ProfesorMapper.listDTOToEntity(profesoriDTO);
 
             List<Profesor> filteredProfesori = profesori.stream()
                     .filter(profesor -> profesor.getGradDidactic() == gradDidactic)
                     .collect(Collectors.toList());
 
-            if (filteredProfesori.isEmpty()) {
-                throw new ResourceNotFoundException("Profesor", "gradDidactic", acad_rank);
-            }
-
-            return filteredProfesori;
+            return ProfesorMapper.listEntityToDTO(filteredProfesori);
         } catch (IllegalArgumentException e) {
-            throw new EnumException("acad_rank", acad_rank);
+            throw new RequestParamWrong("acad_rank",acad_rank,"gradul didactic este invalid");
         }
     }
 
-    public List<Profesor> findProfesoriByName(String nume) {
+    public List<ProfesorDTO> findProfesoriByName(String nume) {
         List<Profesor> profesori = profesorRepo.findByNumeStartingWithOrPrenumeStartingWith(nume, nume);
 
-        if (profesori.isEmpty()) {
-            throw new ResourceNotFoundException("Profesor", "name like", nume);
-        }
-
-        return profesori;
+        return ProfesorMapper.listEntityToDTO(profesori);
     }
 
-    public ProfesorDTO addProfesor(ProfesorDTO profesorDTO)
-    {
-        Profesor profesor= ProfesorMapper.toEntity(profesorDTO);
+    public ProfesorDTO addProfesor(ProfesorDTO profesorDTO) {
+        Profesor profesor = ProfesorMapper.toEntity(profesorDTO);
 
         if (profesorRepo.existsByEmail(profesor.getEmail())) {
-            throw new UniqueKeyException("Profesori",profesor.getEmail());
+            throw new UniqueKeyException("Profesori", profesor.getEmail());
         }
 
         return ProfesorMapper.toDTO(profesorRepo.save(profesor));
     }
 
+    public List<ProfesorDTO> filterProfesoriByAnStudii(List<ProfesorDTO> profesori, String anStudii) {
+        try {
+            int an = Integer.parseInt(anStudii);
+
+            if (an > 4 || an < 1)
+                throw new IllegalArgumentException("year must be between 1 and 4.");
+
+            List<Profesor> profesorEntity = ProfesorMapper.listDTOToEntity(profesori);
+
+            List<Profesor> filteredProfesori = profesorEntity.stream()
+                    .filter(profesor -> profesor.getDiscipline() != null)
+                    .filter(profesor -> profesor.getDiscipline().stream()
+                            .anyMatch(disciplina -> disciplina.getAnStudiu() == an)) // Compară anul
+                    .collect(Collectors.toList());
+
+            return ProfesorMapper.listEntityToDTO(filteredProfesori);
+
+
+        } catch (NumberFormatException ex) {
+            throw new RequestParamWrong("anStudii", anStudii, "year must be a valid integer.");
+        } catch (IllegalArgumentException ex) {
+            throw new RequestParamWrong("anStudii", anStudii, ex.getMessage());
+        }
+    }
 }
