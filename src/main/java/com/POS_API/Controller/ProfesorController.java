2cb8505602c00ed14ad43a577a1588d8b2ba2d4b
Stefan <stefanromascu030405@gmail.com>
2024-11-18 23:02:02 +0200

    Added paginated view

--- src/main/java/com/POS_API/Controller/ProfesorController.java
+++ src/main/java/com/POS_API/Controller/ProfesorController.java
@@ -1,6 +1,7 @@
 package com.POS_API.Controller;
 
 import com.POS_API.DTO.ProfesorDTO;
+import com.POS_API.Helper.HelperFunctions;
 import com.POS_API.Model.Disciplina;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
@@ -36,11 +37,14 @@ public class ProfesorController {
     }
 
     @GetMapping
-    public ResponseEntity<CollectionModel<EntityModel<Profesor>>> findAllProfesori(
+    public ResponseEntity<CollectionModel<EntityModel<ProfesorDTO>>> findAllProfesori(
             @RequestParam(required = false) String acad_rank,
-            @RequestParam(required = false) String nume) {
+            @RequestParam(required = false) String nume,
+            @RequestParam(required = false) String an_studiu,
+            @RequestParam(required = false, defaultValue = "0") String page,
+            @RequestParam(required = false, defaultValue = "10") String items_per_page) {
 
-        List<Profesor> profesori;
+        List<ProfesorDTO> profesori;
         if (nume != null) {
             profesori = profesorService.findProfesoriByName(nume);
         } else {
@@ -51,23 +55,53 @@ public class ProfesorController {
             profesori = profesorService.filterProfesoriByRank(profesori, acad_rank);
         }
 
-        List<EntityModel<Profesor>> profesorModels = profesori.stream()
+        if (an_studiu != null) {
+            profesori= profesorService.filterProfesoriByAnStudii(profesori, an_studiu);
+        }
+
+        int totalItems = profesori.size();
+        int integerItemPerPage = HelperFunctions.stringToInt(items_per_page,"items_per_page");
+        int integerPage = HelperFunctions.stringToInt(page,"page");
+
+        int fromIndex = Math.min(integerPage * integerItemPerPage, totalItems);
+        int toIndex = Math.min(fromIndex + integerItemPerPage, totalItems);
+
+        List<ProfesorDTO> paginatedProfesori = profesori.subList(fromIndex, toIndex);
+
+        List<EntityModel<ProfesorDTO>> profesorModels = paginatedProfesori.stream()
                 .map(profesor -> EntityModel.of(profesor,
                         linkTo(methodOn(ProfesorController.class).findProfesorById(profesor.getId())).withSelfRel(),
                         linkTo(methodOn(ProfesorController.class).findDisciplinaByProfesorId(profesor.getId())).withRel("lectures")))
                 .collect(Collectors.toList());
 
-        Link selfLink = linkTo(methodOn(ProfesorController.class).findAllProfesori(acad_rank, nume)).withSelfRel();
-        CollectionModel<EntityModel<Profesor>> collectionModel = CollectionModel.of(profesorModels, selfLink);
+        Link selfLink = linkTo(methodOn(ProfesorController.class).findAllProfesori(acad_rank, nume, an_studiu,page,items_per_page)).withSelfRel();
+        CollectionModel<EntityModel<ProfesorDTO>> collectionModel = CollectionModel.of(profesorModels, selfLink);
+
+        collectionModel.add(
+                linkTo(methodOn(ProfesorController.class).findAllProfesori(acad_rank, nume, an_studiu,Integer.toString(integerPage), Integer.toString(integerItemPerPage)))
+                        .withRel("current_page")
+        );
+        if (fromIndex > 0) {
+            collectionModel.add(
+                    linkTo(methodOn(ProfesorController.class).findAllProfesori(acad_rank, nume, an_studiu,Integer.toString(integerPage-1), Integer.toString(integerItemPerPage)))
+                            .withRel("previous_page")
+            );
+        }
+        if (toIndex < totalItems) {
+            collectionModel.add(
+                    linkTo(methodOn(ProfesorController.class).findAllProfesori(acad_rank, nume, an_studiu,Integer.toString(integerPage+1), Integer.toString(integerItemPerPage)))
+                            .withRel("next_page")
+            );
+        }
 
         return ResponseEntity.ok(collectionModel);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<EntityModel<Profesor>> findProfesorById(@PathVariable int id) {
-        Profesor profesor = profesorService.findProfesorById(id);
+    public ResponseEntity<EntityModel<ProfesorDTO>> findProfesorById(@PathVariable int id) {
+        ProfesorDTO profesor = profesorService.findProfesorById(id);
 
-        EntityModel<Profesor> profesorJSON = EntityModel.of(profesor);
+        EntityModel<ProfesorDTO> profesorJSON = EntityModel.of(profesor);
 
         Link selfLink = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(ProfesorController.class).findProfesorById(id)).withSelfRel();
         Link findDisciplinaLink = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(ProfesorController.class).findDisciplinaByProfesorId(id)).withRel("findDisciplinaByProfesorId");
@@ -91,7 +125,7 @@ public class ProfesorController {
 
         CollectionModel<EntityModel<Disciplina>> collectionModel = CollectionModel.of(disciplinaModels,
                 linkTo(methodOn(ProfesorController.class).findProfesorById(id)).withRel("profesor"),
-                linkTo(methodOn(ProfesorController.class).findAllProfesori(null, null)).withRel("allProfessors"));
+                linkTo(methodOn(ProfesorController.class).findAllProfesori(null, null,null,null,null)).withRel("allProfessors"));
 
         return ResponseEntity.ok(collectionModel);
     }
