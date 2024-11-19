5c295e0bd974e8fc28a721a3de71d000db2a97e8
Stefan <stefanromascu030405@gmail.com>
2024-11-19 18:36:10 +0200

    Added paginated view

--- src/main/java/com/POS_API/Controller/DisciplinaController.java
+++ src/main/java/com/POS_API/Controller/DisciplinaController.java
@@ -1,6 +1,7 @@
 package com.POS_API.Controller;
 
 import com.POS_API.DTO.DisciplinaDTO;
+import com.POS_API.Helper.HelperFunctions;
 import com.POS_API.Service.DisciplinaService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,7 +31,9 @@ public class DisciplinaController {
     @GetMapping
     public ResponseEntity<CollectionModel<EntityModel<DisciplinaDTO>>> findAllDiscipline(
             @RequestParam(required = false) String type,
-            @RequestParam(required = false) String category) {
+            @RequestParam(required = false) String category,
+            @RequestParam(required = false, defaultValue = "0") String page,
+            @RequestParam(required = false, defaultValue = "10") String items_per_page) {
 
         List<DisciplinaDTO> discipline = disciplinaService.findAllDiscipline();
 
@@ -42,17 +45,44 @@ public class DisciplinaController {
             discipline = disciplinaService.filterDisciplineByCategory(discipline, category);
         }
 
-        List<EntityModel<DisciplinaDTO>> disciplineModels = discipline.stream()
+        int totalItems = discipline.size();
+        int integerItemPerPage = HelperFunctions.stringToInt(items_per_page, "items_per_page");
+        int integerPage = HelperFunctions.stringToInt(page, "page");
+
+        int fromIndex = Math.min(integerPage * integerItemPerPage, totalItems);
+        int toIndex = Math.min(fromIndex + integerItemPerPage, totalItems);
+
+        List<DisciplinaDTO> paginatedDiscipline = discipline.subList(fromIndex, toIndex);
+
+        List<EntityModel<DisciplinaDTO>> disciplineModels = paginatedDiscipline.stream()
                 .map(disciplina -> EntityModel.of(disciplina,
                         linkTo(methodOn(DisciplinaController.class).findDisciplinaByCod(disciplina.getCod())).withSelfRel(),
-                        linkTo(methodOn(DisciplinaController.class).findAllDiscipline(null, null)).withRel("all-lectures")))
+                        linkTo(methodOn(DisciplinaController.class).findAllDiscipline(null, null, page, items_per_page)).withRel("all-lectures")))
                 .collect(Collectors.toList());
 
-        Link selfLink = linkTo(methodOn(DisciplinaController.class).findAllDiscipline(type, category)).withSelfRel();
+        Link selfLink = linkTo(methodOn(DisciplinaController.class).findAllDiscipline(type, category, page, items_per_page)).withSelfRel();
         CollectionModel<EntityModel<DisciplinaDTO>> collectionModel = CollectionModel.of(disciplineModels, selfLink);
+
+        collectionModel.add(
+                linkTo(methodOn(DisciplinaController.class).findAllDiscipline(type, category, Integer.toString(integerPage), Integer.toString(integerItemPerPage)))
+                        .withRel("current_page")
+        );
+        if (fromIndex > 0) {
+            collectionModel.add(
+                    linkTo(methodOn(DisciplinaController.class).findAllDiscipline(type, category, Integer.toString(integerPage - 1), Integer.toString(integerItemPerPage)))
+                            .withRel("previous_page")
+            );
+        }
+        if (toIndex < totalItems) {
+            collectionModel.add(
+                    linkTo(methodOn(DisciplinaController.class).findAllDiscipline(type, category, Integer.toString(integerPage + 1), Integer.toString(integerItemPerPage)))
+                            .withRel("next_page")
+            );
+        }
 
         return ResponseEntity.ok(collectionModel);
     }
+
 
     @GetMapping("/{cod}")
     public ResponseEntity<EntityModel<DisciplinaDTO>> findDisciplinaByCod(@PathVariable String cod) {
@@ -60,7 +90,7 @@ public class DisciplinaController {
 
         EntityModel<DisciplinaDTO> disciplinaModel = EntityModel.of(disciplina,
                 linkTo(methodOn(DisciplinaController.class).findDisciplinaByCod(cod)).withSelfRel(),
-                linkTo(methodOn(DisciplinaController.class).findAllDiscipline(null, null)).withRel("all-lectures"));
+                linkTo(methodOn(DisciplinaController.class).findAllDiscipline(null, null,null,null)).withRel("all-lectures"));
 
         return ResponseEntity.ok(disciplinaModel);
     }
