e4a585a9e3fa01a5a50f221348161443411b84c5
Stefan <stefanromascu030405@gmail.com>
2024-11-19 17:20:39 +0200

    Created class

---
+++ src/main/java/com/POS_API/DTO/DisciplinaDTO.java
@@ -0,0 +1,37 @@
+package com.POS_API.DTO;
+
+import com.POS_API.Model.Student;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class DisciplinaDTO {
+
+    @JsonIgnore
+    private String cod;
+
+    @NotBlank(message = "Numele disciplinei nu poate sa fie gol")
+    private String numeDisciplina;
+
+    @NotBlank(message = "Disciplina trebuie sa aiba un an de studiu")
+    private String anStudiu;
+
+    @NotBlank(message = "Tipul disciplinei nu poate sa fie gol")
+    private String tipDisciplina;
+
+    @NotBlank(message = "Categoria disciplinei nu poate sa fie gol")
+    private String categorieDisciplina;
+
+    @NotBlank(message = "Tipul examinarii nu poate sa fie gol")
+    private String tipExaminare;
+
+    @JsonIgnore
+    private List<Student> studenti;
+
+}
