193f40fc3d1a7520832ebc40de8b4b3197009e8d
Stefan <stefanromascu030405@gmail.com>
2024-12-04 18:36:35 +0200

    initial commit for mongoDB service

---
+++ MONGO/src/main/java/com/POS_API_MONGO/PosApiMongoApplication.java
@@ -0,0 +1,13 @@
+package com.POS_API_MONGO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosApiMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosApiMongoApplication.class, args);
+	}
+
+}
---
+++ MONGO/src/test/java/com/POS_API_MONGO/PosApiMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.POS_API_MONGO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosApiMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
