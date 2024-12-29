95b05e3b4d527b6aad471ac83b19579f33630b7c
Stefan <stefanromascu030405@gmail.com>
2024-12-29 18:54:13 +0200

    Create config file

---
+++ SQL/src/main/java/com/POS_API/Config/GrpcClientConfig.java
@@ -0,0 +1,24 @@
+package com.POS_API.Config;
+
+import auth.AuthServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GrpcClientConfig {
+
+    @Bean
+    public ManagedChannel grpcChannel() {
+        return ManagedChannelBuilder.forAddress("localhost", 50051)
+                .usePlaintext()
+                .build();
+    }
+
+    @Bean
+    public AuthServiceGrpc.AuthServiceBlockingStub authServiceBlockingStub(ManagedChannel grpcChannel) {
+        return AuthServiceGrpc.newBlockingStub(grpcChannel);
+    }
+}
