ad50e12f93b6bf1c3329284818e50a88e50ed99f
Stefan <stefanromascu030405@gmail.com>
2025-01-01 21:26:41 +0200

    Added logic for student detail based on JWT

--- SQL/src/main/java/com/POS_API/Service/StudentService.java
+++ SQL/src/main/java/com/POS_API/Service/StudentService.java
@@ -1,23 +1,21 @@
 package com.POS_API.Service;
 
+import com.POS_API.Advice.Exception.IdmServiceException;
 import com.POS_API.Advice.Exception.ResourceNotFoundException;
 import com.POS_API.Advice.Exception.UniqueKeyException;
 import com.POS_API.DTO.DisciplinaDTO;
 import com.POS_API.DTO.StudentDTO;
 import com.POS_API.Mapper.DisciplinaMapper;
-import com.POS_API.Mapper.ProfesorMapper;
 import com.POS_API.Mapper.StudentMapper;
 import com.POS_API.Model.Disciplina;
 import com.POS_API.Model.Student;
 import com.POS_API.Repository.StudentDAO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 public class StudentService {
@@ -36,10 +34,10 @@ public class StudentService {
     }
 
     public StudentDTO findStudentById(int id) {
-        Optional<Student> student= studentRepo.findStudentById(id);
+        Optional<Student> student = studentRepo.findStudentById(id);
 
-        if(student.isEmpty())
-            throw new ResourceNotFoundException("Student","id",id);
+        if (student.isEmpty())
+            throw new ResourceNotFoundException("Student", "id", id);
 
         return StudentMapper.toDTO(student.get());
     }
@@ -53,15 +51,33 @@ public class StudentService {
         return DisciplinaMapper.listToDTO(disciplinaList);
     }
 
-    public StudentDTO addStudent(StudentDTO studentDTO)
-    {
+    public List<DisciplinaDTO> getDisciplineForStudentByProfessor(int studentId, String profEmail) {
+        List<Disciplina> studentDiscipline = studentRepo.findById(studentId)
+                .map(Student::getDiscipline)
+                .orElseThrow(() -> new ResourceNotFoundException("Student", "id", studentId));
+
+        List<Disciplina> filteredDiscipline = studentDiscipline.stream()
+                .filter(d -> d.getTitular() != null && d.getTitular().getEmail().equals(profEmail))
+                .toList();
+
+        return DisciplinaMapper.listToDTO(filteredDiscipline);
+    }
+
+    public StudentDTO addStudent(StudentDTO studentDTO) {
         Student student = StudentMapper.toEntity(studentDTO);
 
-        if(studentRepo.existsByEmail(student.getEmail())){
+        if (studentRepo.existsByEmail(student.getEmail())) {
             throw new UniqueKeyException("Studenti", student.getEmail());
         }
 
         return StudentMapper.toDTO(studentRepo.save(student));
+    }
+
+    public boolean isSelfReq(int id, String email) {
+        if (studentRepo.existsByIdAndEmail(id, email))
+            return true;
+
+        throw new IdmServiceException(HttpStatus.FORBIDDEN, "FORBIDDEN");
     }
 }
 
