e401cedf148bfed4f70217feb0d2194180ba408b
Stefan <stefanromascu030405@gmail.com>
2025-01-01 12:55:15 +0200

    Added logic to extract token from header Added static variables for Role enum

--- SQL/src/main/java/com/POS_API/Helper/HelperFunctions.java
+++ SQL/src/main/java/com/POS_API/Helper/HelperFunctions.java
@@ -1,12 +1,19 @@
 package com.POS_API.Helper;
 
+import auth.AuthServiceOuterClass;
+import com.POS_API.Advice.Exception.IdmServiceException;
 import com.POS_API.Advice.Exception.RequestParamWrong;
-
-import java.io.Console;
-import java.io.PrintWriter;
+import org.springframework.http.HttpStatus;
 
 public class HelperFunctions {
-    public static int stringToInt(String s,String stringName) {
+
+    public final static AuthServiceOuterClass.Role ADMIN = AuthServiceOuterClass.Role.admin;
+    public final static AuthServiceOuterClass.Role PROFESOR = AuthServiceOuterClass.Role.profesor;
+    public final static AuthServiceOuterClass.Role STUDENT = AuthServiceOuterClass.Role.student;
+
+
+
+    public static int stringToInt(String s, String stringName) {
         int number;
         if (s == null || s.trim().isEmpty()) {
             throw new RequestParamWrong(stringName, s, "The parameter must not be null or empty");
@@ -14,9 +21,27 @@ public class HelperFunctions {
         try {
             number = Integer.parseInt(s);
         } catch (NumberFormatException ex) {
-            throw new RequestParamWrong(stringName,s,"The parameter must be a valid integer");
+            throw new RequestParamWrong(stringName, s, "The parameter must be a valid integer");
         }
 
         return number;
     }
+
+    public static String extractToken(String authorizationHeader) {
+        final String errorMessage = "Authorization header is missing or invalid: ";
+
+        if (authorizationHeader == null) {
+            throw new IdmServiceException(HttpStatus.UNAUTHORIZED, errorMessage + "header is null");
+        }
+        if (authorizationHeader.trim().isEmpty()) {
+            throw new IdmServiceException(HttpStatus.UNAUTHORIZED, errorMessage + "header is empty");
+        }
+        if (!authorizationHeader.startsWith("Bearer ")) {
+            throw new IdmServiceException(HttpStatus.UNAUTHORIZED, errorMessage + "header must start with 'Bearer '");
+        }
+
+        return authorizationHeader.substring(7);
+    }
+
+
 }
