4bb5da18cb2a4727a3dd4c07a49ebd3088822ced
Stefan <stefanromascu030405@gmail.com>
2025-01-11 21:30:46 +0200

    Added logic to save a valid token and reuse it

---
+++ MONGO/src/main/java/com/POS_API_MONGO/Helper/TokenValidator.java
@@ -0,0 +1,30 @@
+package com.POS_API_MONGO.Helper;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class TokenValidator {
+    private static LocalDate savedDate;
+    private static String jwt="";
+
+    public static boolean isTokenValid() {
+        if (jwt.trim().isEmpty())
+            return false;
+
+        LocalDate now = LocalDate.now();
+        if (ChronoUnit.HOURS.between(savedDate.atStartOfDay(), now.atStartOfDay()) >= 7)
+            return false;
+
+        return true;
+    }
+
+    public static void setToken(String newJwt) {
+        jwt = newJwt;
+        savedDate = LocalDate.now();
+    }
+
+    public static String getToken() {
+        return jwt;
+    }
+
+}
