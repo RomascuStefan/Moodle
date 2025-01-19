d6b1ca2584c2b44c3194bc03d52fed08155935a8
Stefan <stefanromascu030405@gmail.com>
2025-01-19 11:57:45 +0200

    new route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import IndexPage from './components/index';
 import DisciplinaPage from './components/disciplina';
 import LoginPage from './components/LoginForm';
+import YourDisciplinesPage from './components/yourDisciplinePage';
+
 
 const App = () => {
   return (
@@ -10,6 +12,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<IndexPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/your-disciplines" element={<YourDisciplinesPage />} />
         <Route path="/disciplina/:cod" element={<DisciplinaPage />} />
       </Routes>
     </Router>
