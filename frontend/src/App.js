d1cf1a2f37b7cd7c4b9e9f84fddf4b119610b0aa
Stefan <stefanromascu030405@gmail.com>
2025-01-19 13:11:50 +0200

    home button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import IndexPage from './components/index';
 import DisciplinaPage from './components/disciplina';
 import LoginPage from './components/LoginForm';
@@ -7,23 +7,28 @@ import YourDisciplinesPage from './components/yourDisciplinePage';
 import UploadFilePage from './components/uploadFiles';
 import CreateDisciplinaPage from './components/createDisciplinaPage';
 
-
-
-
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<IndexPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/your-disciplines" element={<YourDisciplinesPage />} />
-        <Route path="/disciplina/:cod" element={<DisciplinaPage />} />
-        <Route path="/disciplina/:cod/upload" element={<UploadFilePage />} />
-        <Route path="/create-disciplina" element={<CreateDisciplinaPage />} />
-      </Routes>
+      <div>
+        <header>
+          <nav>
+            <Link to="/" style={{ marginRight: '10px' }}>Home</Link>
+          </nav>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<IndexPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/your-disciplines" element={<YourDisciplinesPage />} />
+            <Route path="/disciplina/:cod" element={<DisciplinaPage />} />
+            <Route path="/disciplina/:cod/upload" element={<UploadFilePage />} />
+            <Route path="/create-disciplina" element={<CreateDisciplinaPage />} />
+          </Routes>
+        </main>
+      </div>
     </Router>
   );
 };
 
 export default App;
-
