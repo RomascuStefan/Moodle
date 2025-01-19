de49aad6c24ac13d3a3e3ad8e5962d356cc8987b
Stefan <stefanromascu030405@gmail.com>
2025-01-19 10:52:17 +0200

    CORS

--- MONGO/src/main/java/com/POS_API_MONGO/Controller/MaterieController.java
+++ MONGO/src/main/java/com/POS_API_MONGO/Controller/MaterieController.java
@@ -24,6 +24,7 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
+@CrossOrigin(origins = "${frontend.origin}",exposedHeaders = "Authorization")
 @RequestMapping("/api/academia_mongo")
 public class MaterieController {
 
