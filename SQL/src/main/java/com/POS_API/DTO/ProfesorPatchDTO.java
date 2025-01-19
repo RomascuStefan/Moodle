a0d3be2f26dc5f89da58e4603d0d06f62a2205b0
Stefan <stefanromascu030405@gmail.com>
2025-01-19 19:58:05 +0200

    created DTO for patch request

---
+++ SQL/src/main/java/com/POS_API/DTO/ProfesorPatchDTO.java
@@ -0,0 +1,17 @@
+package com.POS_API.DTO;
+
+import lombok.Data;
+
+@Data
+public class ProfesorPatchDTO {
+    private String nume;
+
+    private String prenume;
+
+    private String gradDidactic;
+
+    private String tipAsociere;
+
+    private String afiliere;
+
+}
---
+++ SQL/src/main/java/com/POS_API/DTO/StudentPatchDTO.java
@@ -0,0 +1,16 @@
+package com.POS_API.DTO;
+
+import lombok.Data;
+
+@Data
+public class StudentPatchDTO {
+    private String nume;
+
+    private String prenume;
+
+    private String cicluStudii;
+
+    private String anStudiu;
+
+    private String grupa;
+}
